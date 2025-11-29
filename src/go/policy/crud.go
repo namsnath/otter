c296c6c186ca51ea7eddbdc02c4f84a83a5bf901
Namit Nathwani <namsnath@gmail.com>
2025-11-29 11:51:58 +0530

    fix: outgoing edges from policy node
    - Uses a single policy node per policy and creates multiple edges corresponding to specifiers

--- src/go/policy/crud.go
+++ src/go/policy/crud.go
@@ -3,33 +3,29 @@ package policy
 import "github.com/namsnath/otter/db"
 
 func (policy Policy) Create() Policy {
-	// TODO: Change this so that the same policy is used for all specifiers in the group. Unwind and create only policy -> specifier relationships in loop
 	query := `
 		MATCH (subject:Subject {name: $subjectName})
 		MATCH (resource:Resource {name: $resourceName})
-		MATCH (specifier:Specifier {key: $specifierKey, value: $specifierValue})
 		CREATE (policy:Policy {id: randomUUID()})
 		CREATE (subject)-[:HAS_POLICY]->(policy)<-[:HAS_POLICY]-(resource)
+
+		WITH policy
+		UNWIND keys($specifiers) AS k
+		MATCH (specifier:Specifier {key: k, value: $specifiers[k]})
 		CREATE (policy)-[e:$($action)]->(specifier)
 	`
 	params := map[string]any{
 		"subjectName":  policy.Subject.Name,
 		"resourceName": policy.Resource.Name,
 		"action":       string(policy.Action),
+		"specifiers":   policy.Specifiers.AsMap(),
 	}
 
 	if len(policy.Specifiers.Specifiers) == 0 {
-		// No specifiers, create policy using the root specifier
-		params["specifierKey"] = "*"
-		params["specifierValue"] = "*"
-		db.ExecuteQuery(query, params)
-	} else {
-		for _, specifier := range policy.Specifiers.Specifiers {
-			params["specifierKey"] = specifier.Key
-			params["specifierValue"] = specifier.Value
-			db.ExecuteQuery(query, params)
-		}
+		params["specifiers"] = map[string]string{"*": "*"}
 	}
+
+	db.ExecuteQuery(query, params)
 
 	return policy
 }
